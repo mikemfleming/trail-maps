357ae190792ff2f0d05ef019ad2e47a7f229c9dd
mikemfleming <mike.fleming@springbox.com>
2019-06-11 13:42:50 -0500

    temp commit

--- example.tf
+++ example.tf
@@ -22,6 +22,11 @@ resource "aws_iam_role" "trail_maps_lambda" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "trail_maps_lambda_log_policy_attachment" {
+  role = "${aws_iam_role.trail_maps_lambda.name}"
+  policy_arn = "${aws_iam_policy.trail_maps_log_policy.arn}"
+}
+
 resource "aws_lambda_function" "trail-maps-email-handler" {
   filename      = "lambda.zip"
   function_name = "trail-maps-email-handler"
@@ -30,4 +35,27 @@ resource "aws_lambda_function" "trail-maps-email-handler" {
   source_code_hash = "${filebase64sha256("lambda.zip")}"
 
   runtime = "nodejs10.x"
+}
+
+resource "aws_iam_policy" "trail_maps_log_policy" {
+  name = "trail_maps_log_policy"
+  path = "/"
+  description = "IAM policy for logging from trail maps lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:CreateLogGroup"
+      ],
+      "Resource": "${aws_lambda_function.trail-maps-email-handler.logging.arn}",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
 }
