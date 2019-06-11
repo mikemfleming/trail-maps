f858cf611070bad9042f9874b7bb9fee273343a3
miek <mike.fleming.dev@gmail.com>
2019-06-11 16:33:08 -0500

    configures lambda logging

--- aws.tf
+++ aws.tf
@@ -27,11 +27,11 @@ resource "aws_iam_role_policy_attachment" "trail_maps_lambda_log_policy_attachme
   policy_arn = "${aws_iam_policy.trail_maps_log_policy.arn}"
 }
 
-resource "aws_lambda_function" "trail-maps-email-handler" {
-  filename      = "lambda.zip"
-  function_name = "trail-maps-email-handler"
-  role          = "${aws_iam_role.trail_maps_lambda.arn}"
-  handler       = "exports.handleEmail"
+resource "aws_lambda_function" "trail_maps_email_handler" {
+  filename = "lambda.zip"
+  function_name = "trail_maps_email_handler"
+  role = "${aws_iam_role.trail_maps_lambda.arn}"
+  handler = "exports.handleEmail"
   source_code_hash = "${filebase64sha256("lambda.zip")}"
 
   runtime = "nodejs10.x"
@@ -52,7 +52,7 @@ resource "aws_iam_policy" "trail_maps_log_policy" {
         "logs:PutLogEvents",
         "logs:CreateLogGroup"
       ],
-      "Resource": "${aws_lambda_function.trail-maps-email-handler.logging.arn}",
+      "Resource": "arn:aws:logs:*:*:*",
       "Effect": "Allow"
     }
   ]
