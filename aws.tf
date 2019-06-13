a07039433408835af1653fc4843d1f89c13a6c14
miek <mike.fleming.dev@gmail.com>
2019-06-12 21:24:14 -0500

    adds notes

--- aws.tf
+++ aws.tf
@@ -1,7 +1,9 @@
+# Meta info about the deployment
 provider "aws" {
   region = "us-east-1"
 }
 
+# Creates a role for the Lambda
 resource "aws_iam_role" "trail_maps_lambda" {
   name = "trail_maps_lambda"
 
@@ -22,11 +24,13 @@ resource "aws_iam_role" "trail_maps_lambda" {
 EOF
 }
 
+# Attaches the above role to the Lambda
 resource "aws_iam_role_policy_attachment" "trail_maps_lambda_log_policy_attachment" {
   role = "${aws_iam_role.trail_maps_lambda.name}"
   policy_arn = "${aws_iam_policy.trail_maps_log_policy.arn}"
 }
 
+# Creates the Lambda with the .zip from the lambda directory
 resource "aws_lambda_function" "trail_maps_email_handler" {
   filename = "lambda.zip"
   function_name = "trail_maps_email_handler"
@@ -37,6 +41,7 @@ resource "aws_lambda_function" "trail_maps_email_handler" {
   runtime = "nodejs10.x"
 }
 
+# Creates a role for the Lambda to write logs
 resource "aws_iam_policy" "trail_maps_log_policy" {
   name = "trail_maps_log_policy"
   path = "/"
@@ -59,3 +64,14 @@ resource "aws_iam_policy" "trail_maps_log_policy" {
 }
 EOF
 }
+
+# TODO: 
+#   - Configure SES https://www.terraform.io/docs/providers/aws/r/ses_domain_identity.html
+#       * with bucket and policy to write to it
+#       * with sns topic
+#   - Configure DynamoDB https://www.terraform.io/docs/providers/aws/r/dynamodb_table.html
+#       * with policy that allows queries from an ip 
+#   - Trigger Lambda from that SNS topic to
+#       * scan all the emails in that bucket (will need a policy for this)
+#       * parse the emails into lat/lng and message
+#       * write that data to DDB
--- lambda/exports.js
+++ lambda/exports.js
@@ -1,18 +1,6 @@
-exports.handleEmail = function(event, context, callback) {
-  console.log('Spam filter');
-  
-  var sesNotification = event.Records[0].ses;
-  console.log("SES Notification:\n", JSON.stringify(sesNotification, null, 2));
 
-  // Check if any spam check failed
-  if (sesNotification.receipt.spfVerdict.status === 'FAIL'
-          || sesNotification.receipt.dkimVerdict.status === 'FAIL'
-          || sesNotification.receipt.spamVerdict.status === 'FAIL'
-          || sesNotification.receipt.virusVerdict.status === 'FAIL') {
-      console.log('Dropping spam');
-      // Stop processing rule set, dropping message
-      callback(null, {'disposition':'STOP_RULE_SET'});
-  } else {
-      callback(null, null);   
-  }
+exports.handleEmail = function(event, context, callback) {
+  var message = event.Records[0].Sns.Message;
+  console.log('Message received from SNS:', message); 
+  callback(null, "Success");
 };
