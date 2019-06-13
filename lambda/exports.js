96c5759de72369a8033f96388f03b5042e5f1a04
miek <mike.fleming.dev@gmail.com>
2019-06-13 16:01:19 -0500

    reads files from s3

--- lambda/exports.js
+++ lambda/exports.js
@@ -1,6 +1,57 @@
+const s3 = require('aws-sdk/clients/s3');
+const parse = require('parse-email');
 
-exports.handleEmail = function(event, context, callback) {
-  var message = event.Records[0].Sns.Message;
-  console.log('Message received from SNS:', message); 
-  callback(null, "Success");
+const s3Client = new s3();
+
+function getFileKeys () {
+  return new Promise((resolve, reject) => {
+    s3Client.listObjects({ Bucket: 'trail-maps-ses-bucket' }, (err ,data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  }).then(({ Contents }) => Contents.map(o => o.Key));
+}
+
+function getFileFromS3 (key) {
+  return new Promise((resolve, reject) => {
+    s3Client.getObject({
+      Bucket: 'trail-maps-ses-bucket',
+      Key: key
+    }, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  }).then(f => f.Body.toString());
+}
+
+// [ 'attachments',
+//   'headers',
+//   'html',
+//   'text',
+//   'textAsHtml',
+//   'subject',
+//   'references',
+//   'date',
+//   'to',
+//   'from',
+//   'messageId',
+//   'inReplyTo' ]
+function parseFile (file) {
+  return parse(file)
+    .then(({ subject, text, date, messageId }) => ({ subject, text, date, messageId }));
+}
+
+exports.handleEmail = async (event, context) => {
+  console.log('~~~~~~~~~~ event', event);
+  console.log('~~~~~~~~~~ context', context);
+
+  const keys = await getFileKeys();
+  const files = await Promise.all(keys.map(getFileFromS3));
+  const parsed = await Promise.all(files.map(parseFile));
+
+  console.log('Message received from SNS:', parsed); 
+
+  return 'Ok';
 };
+
+module.exports.handleEmail()
